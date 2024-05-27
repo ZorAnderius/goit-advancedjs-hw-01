a72f9b319573a952d27dda9edce6ffdaf57565cd
ZorAnderius <gabriel.zorander@gmail.com>
2024-05-27 17:30:24 +0100

    add task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,10 @@
 import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('#vimeo-player');
 const LOCAL_KEY = 'videoplayer-current-time';
+const lodash_time = 1000;
 
 const player = new Player(iframe);
 
@@ -14,4 +17,4 @@ const handlerGetTime = (e) => {
 const currentTime = JSON.parse(localStorage.getItem(LOCAL_KEY)) || 0;
 
 player.setCurrentTime(currentTime);
-player.on('timeupdate', handlerGetTime);
+player.on('timeupdate', throttle(handlerGetTime, lodash_time));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle";
 
+const LOCAL_KEY = 'feedback-form-state';
+const lodash_time = 500;
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    emailInput: document.querySelector('input[name="email"]'),
+    messageInput: document.querySelector('textarea[name="message"]'),
+}
+
+const handlerInput = e => { 
+    const { name, value } = e.target;
+    const feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+    feedback[name] = value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(feedback));
+}
+
+const handlerSubmit = e => {
+    e.preventDefault();
+    const feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+    console.log(feedback);
+    localStorage.removeItem(LOCAL_KEY);
+    refs.formEl.reset();
+}
+const feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+refs.emailInput.value = feedback.email || '';
+refs.messageInput.value = feedback.message || '';
+
+refs.formEl.addEventListener('input', throttle(handlerInput, lodash_time));
+refs.formEl.addEventListener('submit', handlerSubmit);
