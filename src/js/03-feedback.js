82bdc46e84b6b20cdd8a9795724fac45d9d8bd95
ZorAnderius <gabriel.zorander@gmail.com>
2024-05-27 17:40:11 +0100

    change const ro let variabe

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,23 +9,24 @@ const refs = {
     messageInput: document.querySelector('textarea[name="message"]'),
 }
 
+let feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+refs.emailInput.value = feedback.email || '';
+refs.messageInput.value = feedback.message || '';
+
 const handlerInput = e => { 
     const { name, value } = e.target;
-    const feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+    feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
     feedback[name] = value;
     localStorage.setItem(LOCAL_KEY, JSON.stringify(feedback));
 }
 
 const handlerSubmit = e => {
     e.preventDefault();
-    const feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+    feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
     console.log(feedback);
     localStorage.removeItem(LOCAL_KEY);
     refs.formEl.reset();
 }
-const feedback = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
-refs.emailInput.value = feedback.email || '';
-refs.messageInput.value = feedback.message || '';
 
 refs.formEl.addEventListener('input', throttle(handlerInput, lodash_time));
 refs.formEl.addEventListener('submit', handlerSubmit);
