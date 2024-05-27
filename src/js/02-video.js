d3670719fa42ec705cc5ad8a12b7cea6b58c5587
ZorAnderius <gabriel.zorander@gmail.com>
2024-05-27 16:24:47 +0100

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const LOCAL_KEY = 'videoplayer-current-time';
+
+const player = new Player(iframe);
+
+const handlerGetTime = (e) => {
+    const { seconds, duration } = e;
+    if (seconds && duration && seconds >= 0 && seconds <= duration)
+        localStorage.setItem(LOCAL_KEY, JSON.stringify(seconds));
+};
+
+const currentTime = JSON.parse(localStorage.getItem(LOCAL_KEY)) || 0;
+
+player.setCurrentTime(currentTime);
+player.on('timeupdate', handlerGetTime);
